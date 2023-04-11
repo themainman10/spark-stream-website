3bf85be5d8ef91ed875d99c2c04daffb7e112e1f
themainman10 <angelobrown1000@gmail.com>
2023-04-11 09:04:23 -0400

    asd
    dfvafa

---
+++ Index page/script.js
@@ -0,0 +1,4 @@
+// script.js
+document.querySelector('.circular-button').addEventListener('click', () => {
+    document.querySelector('#booking-section').scrollIntoView({ behavior: 'smooth' });
+});
